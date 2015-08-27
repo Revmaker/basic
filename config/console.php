c3bec260c6cb251e4d1ed5d488a9c68d52e00748
Sandy <sganz@pacbell.net>
2015-08-26 19:40:23 -0700

    removed gii dependancy on the console runner, causes a problem for some reason

--- config/console.php
+++ config/console.php
@@ -8,7 +8,7 @@ $db = require(__DIR__ . '/db.php');
 return [
     'id' => 'basic-console',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log', 'gii'],
+    'bootstrap' => ['log'], //, 'gii'], // had to remove to make console work...
     'controllerNamespace' => 'app\commands',
     'modules' => [
         'gii' => 'yii\gii\Module',
@@ -27,5 +27,14 @@ return [
         ],
         'db' => $db,
     ],
+
+	// for user management
+	
+	'modules'=>[
+		'user-management' => [
+			'class' => 'webvimark\modules\UserManagement\UserManagementModule',
+		],
+	],
+
     'params' => $params,
 ];
